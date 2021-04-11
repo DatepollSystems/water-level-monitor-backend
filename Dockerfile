2c0be27b2518ef38cf7ce4e2063ccc046b912ca4
kaulex99 <contact@kaulex.dev>
2021-04-11 16:42:49 +0200

    updated ci

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM openjdk:11.0.8-slim-buster
-ADD build/libs/water-level-monitor-0.0.1-SNAPSHOT.jar water-level-monitor.jar
-RUN apt-get -q update && apt-get -qy install netcat
-ADD https://raw.githubusercontent.com/capripot/wait-for/master/wait-for wait-for.sh
-RUN chmod +x wait-for.sh
+ADD build/libs/water-level-monitor-0.0.1.jar water-level-monitor.jar
 ENTRYPOINT ["java", "-jar", "water-level-monitor.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.water-level-monitor"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -21,17 +21,21 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web");
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.hibernate:hibernate-core:5.4.30.Final")
+    implementation("com.h2database:h2:1.4.200")
+
     runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
     testImplementation("org.springframework.security:spring-security-test")
+    implementation("com.h2database:h2:1.4.200")
 }
 
 tasks.withType<Test> {
